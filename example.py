0450d86a61c0278e7b9d0deab4956c703e2e3bfb
Vincy SHI <vincy@vincy1230.net>
2025-04-21 19:30:29 +0800

    提升 example.py

--- example.py
+++ example.py
@@ -1,8 +1,14 @@
 # 导入课程比较器模块
 import CourseComparator as cc
 
-# 初始化数据加载器
-loader = cc.init("D:\\ProjectFiles\\课程比较器\\Course-Comparator\\data")
+# 传入数据集根目录，初始化数据加载器
+loader = cc.init("<数据集根目录>")
+
+# 获取旧的课程方案
+old_courses = loader("人工智能", "2021", 4)
+
+# 获取新的课程方案
+new_courses = loader("人工智能", "2023", 4)
 
 # 打印两个方案的差异
-print(loader("人工智能", "2021", 8) - loader("人工智能", "2023", 8))
+print(old_courses - new_courses)
