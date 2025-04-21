53ea552622e869b751cb3c32cbc3539b588f2f37
Vincy SHI <vincy@vincy1230.net>
2025-04-21 19:37:14 +0800

    优化示例

--- example.py
+++ example.py
@@ -2,13 +2,13 @@
 import CourseComparator as cc
 
 # 传入数据集根目录，初始化数据加载器
-loader = cc.init("<数据集根目录>")
+loader = cc.init("./data")
 
 # 获取旧的课程方案
-old_courses = loader("人工智能", "2021", 4)
+old_courses = loader("示例", "2022", 4)
 
 # 获取新的课程方案
-new_courses = loader("人工智能", "2023", 4)
+new_courses = loader("示例", "2023", 4)
 
 # 打印两个方案的差异
 print(old_courses - new_courses)
